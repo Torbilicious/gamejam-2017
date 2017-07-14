5d07614611c3ff2a9e41e40b79d4558d4cf3f72c
Marcel Bulla <igjam17@marcel-bulla.de>
2017-07-14 21:51:49 +0200

    Added simple menu system

---
+++ Assets/Scripts/Menu_Behavior/LevelSelectionHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//TODO: Load Levels
+public class LevelSelectionHandler : MonoBehaviour {
+
+    public string MainMenu = "Main_Menu";
+
+	public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(MainMenu, LoadSceneMode.Single);
+    }
+}
