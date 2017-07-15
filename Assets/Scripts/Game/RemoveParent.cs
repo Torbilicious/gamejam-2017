4dd6d74fd119edc80145b58243020d3b13bc923b
Marcel Bulla <igjam11@marcel-bulla.de>
2017-07-15 13:31:39 +0200

    Lemming can no longer have parents :(

---
+++ Assets/Scripts/Game/RemoveParent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveParent : MonoBehaviour
+{
+
+	void Start ()
+    {
+        transform.parent = null;
+	}
+}
