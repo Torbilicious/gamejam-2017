fe19318430bc0947f06b70e18325068b472c0014
Marcel Bulla <igjam11@marcel-bulla.de>
2017-07-15 17:38:59 +0200

    merge fail

--- Assets/Scripts/Game/Tile.cs
+++ Assets/Scripts/Game/Tile.cs
@@ -182,16 +182,6 @@ public class Tile : MonoBehaviour
                     directions[direction] = false;
                 }
             }
-
-            Tile north = LevelModel.Instance.TryGetTile((int)transform.position.z + 1, (int)transform.position.x);
-            Tile south = LevelModel.Instance.TryGetTile((int)transform.position.z - 1, (int)transform.position.x);
-            Tile east = LevelModel.Instance.TryGetTile((int)transform.position.z, (int)transform.position.x + 1);
-            Tile west = LevelModel.Instance.TryGetTile((int)transform.position.z, (int)transform.position.x - 1);
-
-            if (north && north.IsOnFire) directions[RunDirection.North] = false;
-            if (south && south.IsOnFire) directions[RunDirection.South] = false;
-            if (east && east.IsOnFire) directions[RunDirection.East] = false;
-            if (west && west.IsOnFire) directions[RunDirection.West] = false;
         }
         catch { }
         return directions;
