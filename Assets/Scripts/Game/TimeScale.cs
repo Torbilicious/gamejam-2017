8ca205f80f69107c1df41fb0dc75f93516e4074a
(Dev) Ruben <horn.ruben@yahoo.de>
2017-07-16 08:21:50 +0200

    variable time scale

---
+++ Assets/Scripts/Game/TimeScale.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeScale : MonoBehaviour
+{
+    [SerializeField]
+    private float timeScaleFactor = 4;
+
+    [SerializeField]
+    private Sprite textureNormal, textureFastMode;
+
+    [SerializeField]
+    private UnityEngine.UI.Image buttonImage;
+
+    private bool fastMode = false;
+
+    public void ToggleTimeScale()
+    {
+        fastMode = !fastMode;
+        Time.timeScale = fastMode ? timeScaleFactor : 1;
+        buttonImage.sprite = !fastMode ? textureFastMode : textureNormal;
+    }
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+    }
+}
