c6e3531156655873a409a1c3bee1d06a5244c033
AWlochowitz <a.wlochowitz@steinberg.de>
2017-07-16 09:51:20 +0200

    credits

---
+++ Assets/CreditsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsController : MonoBehaviour {
+
+	void Start () {
+        GetComponent<Animator>().SetBool("extinguishing",true);
+        Time.timeScale = 0.5f;
+    }
+	
+    void OnDisable()
+    {
+        Time.timeScale = 1;
+    }
+
+}
