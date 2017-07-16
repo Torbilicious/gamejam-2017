91eb1c1aaacb5583a8c5289fef3af65d37d969a9
(Dev) Ruben <horn.ruben@yahoo.de>
2017-07-16 09:41:31 +0200

    (Attempt) light fix webgl

---
+++ Assets/Scripts/Game/LightSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSpawner : MonoBehaviour
+{
+    public GameObject lightPrefab;
+
+    // Use this for initialization
+    private void Start()
+    {
+        new List<Light>(GameObject.FindObjectsOfType<Light>()).ForEach(s => { if (s.type == LightType.Directional) Destroy(s.gameObject); });
+        Instantiate(lightPrefab);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/Menu_Behavior/Platform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public GameObject exitBtn;
+
+    // Use this for initialization
+    private void Start()
+    {
+#if WEBGL
+        Destroy(exitBtn);
+#endif
+        Screen.fullScreen = true;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
