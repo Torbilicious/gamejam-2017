6985764bf1b98ec92aa5107f49db0337c00ef318
(Dev) Ruben <horn.ruben@yahoo.de>
2017-07-16 13:09:55 +0200

    random lemmings

---
+++ Assets/Scripts/Game/LemmingSkin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LemmingSkin : MonoBehaviour
+{
+    public Material[] lemmingSkins;
+
+    // Use this for initialization
+    private void Start()
+    {
+        Material lemmingMat = lemmingSkins[Random.Range(0, lemmingSkins.Length - 1)];
+        for (int i = 0; i < 6; i++)
+        {
+            transform.GetChild(i).GetComponent<Renderer>().material = lemmingMat;
+        }
+    }
+}
