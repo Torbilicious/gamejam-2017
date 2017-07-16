85899872bdca7ae130af4e864361210b917b4feb
(Dev) Ruben <horn.ruben@yahoo.de>
2017-07-16 11:40:52 +0200

    credits fix

--- Assets/Scripts/Menu_Behavior/MainMenuHandler.cs
+++ Assets/Scripts/Menu_Behavior/MainMenuHandler.cs
@@ -6,23 +6,27 @@ using UnityEngine.SceneManagement;
 /// <summary>
 /// This script handles the main menu behavior (mainly changing scenes on button click)
 /// </summary>
-public class MainMenuHandler : MonoBehaviour {
-
-    public string LevelSelection = "Menu_LevelSelection";
+public class MainMenuHandler : MonoBehaviour
+{
+    public string LevelSelection = "Menu_LevelSelection", LevelCredits = "Credits";
 
     public void Update()
     {
         Camera.main.backgroundColor = new Color(0, 0, 0, 0);
-
     }
+
     public void GoToLevelSelection()
     {
         SceneManager.LoadScene(LevelSelection, LoadSceneMode.Single);
+    }
+
+    public void GoToLevelCredits()
+    {
+        SceneManager.LoadScene(LevelCredits, LoadSceneMode.Single);
     }
 
     public void Exit()
     {
         Application.Quit();
     }
-
 }
